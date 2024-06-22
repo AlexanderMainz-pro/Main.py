473f4a3e46ede6b061c1c3515173b9532c50f6e4
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2024-06-22 18:20:58 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,30 @@
+my_dict = {"Alexander" : 1988,
+           "Broneslav" : 1989,
+           "Igor"      : 1990
+           }
+print(my_dict)
+my_dict["Viktor"] = 1991
+print(my_dict.get("Alexander"), my_dict["Viktor"])
+my_dict["Anatoly"] = 1992
+my_dict["Basil"] = 1993
+del my_dict["Igor"]
+print(my_dict.get("Igor"))
+print(my_dict.get("Igor", 1990))
+print(my_dict)
+
+my_set = {1, 2.0, 2.0, False, "str",(1, 2, 3)}
+print(my_set)
+my_set.update([True, 3])
+my_set.remove(False)
+print(my_set)
+
+
+
+
+
+
+
+
+
+
+
