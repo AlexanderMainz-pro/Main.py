c26794782ef1d5bf90574caf6207b148abb12b43
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2024-07-05 18:57:59 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -1,8 +1,8 @@
-for i in range(3, 21):
-    print(i, end=' ')
-    for j in range(3, 21):
-        f = j // 10
-        s = j % 10
-        if i % (f + s) == 0:
-            print(j, end=' ')
-    print()
+a = int(input("Введите число: "))
+l = []
+for i in range(1, 21):
+    for j in range(1, 21):
+        if a % (i + j) == 0 and i < j:
+            l.append(i)
+            l.append(j)
+    print(l)
