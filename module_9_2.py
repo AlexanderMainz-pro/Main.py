0f7162a4b3da252e6c779ecf191576a6139b0620
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2024-10-14 06:22:24 +0300

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,14 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(string) for string in first_strings if len(string) >= 5]
+
+second_result = [(f, s) for f in first_strings for s in second_strings if len(f) == len(s)]
+
+combined_strings = first_strings + second_strings
+third_result = {string: len(string) for string in combined_strings if len(string) % 2 == 0}
+
+
+print(first_result)
+print(second_result)
+print(third_result)
