63d5ef7b3b34795caeb9350f8b1e75785c0b12cd
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2025-01-15 02:22:51 +0300

    15.01
    15.01

---
+++ main2.py
@@ -0,0 +1,19 @@
+#"1st program"
+print((9 ** 0.5) * 5)
+
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#"3rd program"
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a == b)
+
+#"4th program"
+c = '123.456'
+f = float(c)
+mult = f * 10
+res = int(mult) % 10
+print(res)
